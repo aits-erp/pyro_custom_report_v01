418762b68d0b5a2c25b8adbb72a094fe76991a6d
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-07-01 08:59:57 +0000

    Js Changed

--- pyro_reports/pyro_reports/report/crm_report/crm_report.js
+++ pyro_reports/pyro_reports/report/crm_report/crm_report.js
@@ -29,36 +29,36 @@
 // };
 
 
-frappe.query_reports["crm_report"] = {
-    "filters": [
+frappe.query_reports["CRM Report"] = {
+    filters: [
         {
-            "fieldname": "from_date",
-            "label": "From Date",
-            "fieldtype": "Date",
-            "default": frappe.datetime.month_start(),
-            "reqd": 1
+            fieldname: "from_date",
+            label: "From Date",
+            fieldtype: "Date",
+            default: frappe.datetime.month_start(),
+            reqd: 1
         },
         {
-            "fieldname": "to_date",
-            "label": "To Date",
-            "fieldtype": "Date",
-            "default": frappe.datetime.get_today(),
-            "reqd": 1
+            fieldname: "to_date",
+            label: "To Date",
+            fieldtype: "Date",
+            default: frappe.datetime.get_today(),
+            reqd: 1
         },
         {
-            "fieldname": "customer",
-            "label": "Customer",
-            "fieldtype": "Data"
+            fieldname: "customer",
+            label: "Customer",
+            fieldtype: "Data"
         },
         {
-            "fieldname": "sales_rep",
-            "label": "Sales Rep",
-            "fieldtype": "Data"
+            fieldname: "sales_rep",
+            label: "Sales Rep",
+            fieldtype: "Data"
         },
         {
-            "fieldname": "enq_type",
-            "label": "Enquiry Type",
-            "fieldtype": "Data"
+            fieldname: "enq_type",
+            label: "Enquiry Type",
+            fieldtype: "Data"
         }
     ]
 };
