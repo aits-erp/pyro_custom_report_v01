6c9a9396f45595f00419b1f57a9466766ba3b785
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-07-21 05:37:31 +0000

    Added New Changes in Py

--- pyro_reports/pyro_reports/report/crm_report/crm_report.py
+++ pyro_reports/pyro_reports/report/crm_report/crm_report.py
@@ -842,11 +842,11 @@ def get_data(filters):
     values = {}
 
     if filters.get("from_date"):
-        conditions += " AND op.transaction_date >= %(from_date)s"
+        conditions += " AND COALESCE(op.transaction_date, DATE(op.creation)) >= %(from_date)s"
         values["from_date"] = filters.get("from_date")
 
     if filters.get("to_date"):
-        conditions += " AND op.transaction_date <= %(to_date)s"
+        conditions += " AND COALESCE(op.transaction_date, DATE(op.creation)) <= %(to_date)s"
         values["to_date"] = filters.get("to_date")
 
     if filters.get("sales_rep"):
@@ -872,7 +872,7 @@ def get_data(filters):
 
             ld.custom_enquriry_type as enq_type,
 
-            op.transaction_date as enq_received_on,
+            COALESCE(op.transaction_date, DATE(op.creation)) as enq_received_on,
 
             op.custom_quotation as qtn_no,
 
@@ -955,3 +955,123 @@ def get_data(filters):
     """, values, as_dict=True)
 
     return data
+
+
+# def get_data(filters):
+#     conditions = ""
+#     values = {}
+
+#     if filters.get("from_date"):
+#         conditions += " AND op.transaction_date >= %(from_date)s"
+#         values["from_date"] = filters.get("from_date")
+
+#     if filters.get("to_date"):
+#         conditions += " AND op.transaction_date <= %(to_date)s"
+#         values["to_date"] = filters.get("to_date")
+
+#     if filters.get("sales_rep"):
+#         conditions += " AND op.custom_sales_rep = %(sales_rep)s"
+#         values["sales_rep"] = filters.get("sales_rep")
+
+#     if filters.get("customer"):
+#         conditions += " AND op.customer_name = %(customer)s"
+#         values["customer"] = filters.get("customer")
+
+#     if filters.get("enq_type"):
+#         conditions += " AND ld.custom_enquriry_type = %(enq_type)s"
+#         values["enq_type"] = filters.get("enq_type")
+
+#     data = frappe.db.sql(f"""
+#         SELECT
+
+#             ROW_NUMBER() OVER(ORDER BY op.creation DESC) as sr_no,
+
+#             op.custom_enq_details as enq_details,
+
+#             op.party_name as lead,
+
+#             ld.custom_enquriry_type as enq_type,
+
+#             op.transaction_date as enq_received_on,
+
+#             op.custom_quotation as qtn_no,
+
+#             op.custom_qtn_date as qtn_date,
+
+#             op.custom_offer_qtn_sent_on_date as offer_qtn_sent_on_date,
+
+#             COALESCE(op.customer_name, op.party_name) as customer,
+
+#             op.custom_offer_status_ as offer_status,
+
+#             COALESCE(op.state, ld.state) as place,
+
+#             op.custom_sales_rep as sales_rep,
+
+#             op.custom_project_client as project_client,
+
+#             op.custom_item_product as item_product,
+
+#             op.custom_material_special_design as material_special_design,
+
+#             op.custom_qty_offered as qty_offered,
+
+#             op.custom_offer_value_rs as offer_value_rs,
+
+#             op.expected_closing as expected_in,
+
+#             COALESCE(op.contact_person, ld.lead_name) as customer_contact_person,
+
+#             CONCAT_WS(
+#                 ' / ',
+#                 NULLIF(op.contact_mobile, ''),
+#                 NULLIF(op.phone, ''),
+#                 NULLIF(op.whatsapp, '')
+#             ) as customer_contact_no,
+
+#             COALESCE(op.contact_email, ld.email_id) as customer_email_id,
+
+#             op.custom_po_no as po_no,
+
+#             op.custom_po_date as po_date,
+
+#             op.custom_po_value as po_value,
+
+#             op.custom_po_received as po_received,
+
+#             op.custom_customer_type as custom_customer_type,
+
+#             op.custom_group_unit as custom_group_unit,
+
+#             op.custom_type_of_entity as custom_type_of_entity,
+
+#             op.custom_sales_person as custom_sales_person,
+
+#             op.territory as territory,
+
+#             op.custom_date as custom_date,
+
+#             op.custom_date_follow_up2 as custom_date_follow_up2,
+
+#             op.custom_next_action_to_be_done as custom_next_action_to_be_done,
+
+#             op.custom_follow_up1 as custom_follow_up1,
+
+#             op.custom_follow_up2 as custom_follow_up2,
+
+#             op.custom_outcome as custom_outcome
+
+#         FROM `tabOpportunity` op
+
+#         LEFT JOIN `tabLead` ld
+#         ON ld.name = op.party_name
+#         AND op.opportunity_from = 'Lead'
+
+#         WHERE op.docstatus < 2
+#         {conditions}
+
+#         ORDER BY sr_no ASC
+
+#     """, values, as_dict=True)
+
+#     return data
